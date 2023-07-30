073cfb7099c290066a51b0906e540025ba6ef5bd
laomai <i@laomai.org>
2023-07-30 14:31:21 +0800

    feat: upyun cdn

--- static/js/load-mathjax.js
+++ static/js/load-mathjax.js
@@ -1,12 +1,15 @@
 window.MathJax = {
-    tex: {
-      inlineMath: [['$', '$'], ['\\(', '\\)']]
-    }
-  };
-  
-  (function () {
-    var script = document.createElement('script');
-    script.src = 'https://cdn.qylao.com/dist/mathjax/es5/tex-chtml.js';
-    script.async = true;
-    document.head.appendChild(script);
-  })();
+  tex: {
+    inlineMath: [
+      ['$', '$'],
+      ['\\(', '\\)'],
+    ],
+  },
+};
+
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://npm.elemecdn.com/mathjax@3/es5/tex-chtml.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
