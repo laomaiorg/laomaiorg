bfc49205e4e9602f3dce8196570c0c7470d8fa85
laomai <Unumtu-amd64>
2023-08-17 16:12:56 +0800

    feat: unuse purgecss tailwind dark

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,8 @@
 let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.config.js';
 const tailwind = require('tailwindcss')(tailwindConfig);
 const autoprefixer = require('autoprefixer');
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    './hugo_stats.json'
-  ],
-  defaultExtractor: (content) => {
-    const els = JSON.parse(content).htmlElements;
-    return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
-  },
-  safelist: [],
-});
 
 module.exports = {
 	// eslint-disable-next-line no-process-env
-	plugins: [tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss, autoprefixer] : [])],
+	plugins: [tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])],
 };
