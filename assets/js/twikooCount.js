9cf41e10d05c22ddccd1205ebb00bb7b16ef6212
laomai <Unumtu-amd64>
2023-08-18 09:58:00 +0000

    feat: optimize twikooCount code unsue json and py

--- assets/js/twikooCount.js
+++ assets/js/twikooCount.js
@@ -5,19 +5,19 @@ function updateCommentsCount() {
 
   liElements.forEach(function (li) {
     let relPermalink = li.getAttribute('data-relpermalink')
-    console.log(relPermalink)
+    // console.log(relPermalink)
     let promise = twikoo.getCommentsCount({
       envId: 'https://twikoo.laomai.org',
       urls: [relPermalink],
       includeReply: false, // 评论数是否包括回复，默认：false
     })
-    console.log(promise)
+    // console.log(promise)
     promises.push(promise)
   })
 
   Promise.all(promises)
     .then(function (responses) {
-      console.log(responses)
+      // console.log(responses)
       // 处理每个请求的结果
       responses.forEach(function (res, index) {
         let countElement = document.getElementById(liElements[index].getAttribute('data-relpermalink'))
